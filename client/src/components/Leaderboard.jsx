732c11949b67426417e83ee849e90a994e2fc797
h0s08ln <himani.singh@walmart.com>
2025-06-19 00:15:24 +0530

    added url

--- client/src/components/Leaderboard.jsx
+++ client/src/components/Leaderboard.jsx
@@ -45,7 +45,7 @@ function Leaderboard() {
     };
 
     try {
-      const res = await fetch("https://your-backend-host.com/api/leetcode", {
+      const res = await fetch("https://leetcode-leaderboard-1.onrender.com", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify(query),
